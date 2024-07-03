9ff8879f55b31a05a927b530acb088277a36a481
Kris Lefeber <krislefeber@gmail.com>
2024-07-03 10:18:23 -0500

    added fallback route

--- spring-cloud-gateway/src/main/java/com/example/demo/DemoApplication.java
+++ spring-cloud-gateway/src/main/java/com/example/demo/DemoApplication.java
@@ -7,33 +7,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder) {
+    @Bean
+    public RouteLocator routeV1MoviesToV2Titles(RouteLocatorBuilder builder) {
 
-		return builder.routes()
-				.route("v2", r -> r
-						.path("/api/v1/movies")
-						.filters((f) ->
-							f.rewritePath("/api/v1/movies", "/api/v2/titles")
-									.modifyResponseBody(
-											Title.class,
-											Movie.class,
-											(exc, v2) -> Mono.justOrEmpty(
-													Movie.builder()
-													.name(v2.getTitle())
-													.build()
-											)
-									)
-						).uri("http://service-2:8080"))
-				.build();
-	}
+        return builder.routes()
+                .route("routeV1MoviesToV2Titles", r -> r
+                        .path("/api/v1/movies")
+                        .filters((f) ->
+                                f.rewritePath("/api/v1/movies", "/api/v2/titles")
+                                        .modifyResponseBody(
+                                                Title.class,
+                                                Movie.class,
+                                                (exc, v2) -> Mono.justOrEmpty(
+                                                        Movie.builder()
+                                                                .name(v2.getTitle())
+                                                                .build()
+                                                )
+                                        )
+                        ).uri("http://service-2:8080"))
+                .build();
+    }
+
+
+    @Bean
+    public RouteLocator routeV1Service(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("routeV1Service", r -> r
+                        .order(Ordered.LOWEST_PRECEDENCE)
+                        .path("/api/v1/**")
+                        .uri("http://service-1:8080"))
+                .build();
+    }
 }
